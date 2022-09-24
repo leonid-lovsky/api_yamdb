6dcccc792c3123f9f86a9ed261b1b6bd5f1ba991
Сергей Тюрников <tiurnikovsergei@yandex.ru>
2022-09-24 16:19:33 +0300

    Добалены сериализаторы, ViewSets и urls для моделей Reviw и Comments

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,18 +1,30 @@
 from django.db import models as django_models
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, pagination, viewsets
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import (
+    IsAuthenticatedOrReadOnly
+)
+from django_filters.rest_framework import DjangoFilterBackend
 
-from api import permissions, serializers
-from reviews import models
+from .serializers import (
+    ReviewSerializer,
+    CommentsSerializer,
+    CategorySerializer,
+    TitleSerializer,
+    GenreSerializer
+)
+from reviews.models import Title, Review, Comments, Category, Genre
+
+from api.permissions import IsAdminOrReadOnly
 
 
 class CategoryViewSet(mixins.CreateModelMixin,
                       mixins.DestroyModelMixin,
                       mixins.ListModelMixin,
                       viewsets.GenericViewSet):
-    queryset = models.Category.objects.all()
-    serializer_class = serializers.CategorySerializer
-    permission_classes = (permissions.IsAdminOrReadOnly,)
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAdminOrReadOnly,)
     pagination_class = pagination.LimitOffsetPagination
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
@@ -23,9 +35,9 @@ class GenreViewSet(mixins.CreateModelMixin,
                    mixins.DestroyModelMixin,
                    mixins.ListModelMixin,
                    viewsets.GenericViewSet):
-    queryset = models.Genre.objects.all()
-    serializer_class = serializers.GenreSerializer
-    permission_classes = (permissions.IsAdminOrReadOnly,)
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAdminOrReadOnly,)
     pagination_class = pagination.LimitOffsetPagination
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
@@ -33,14 +45,49 @@ class GenreViewSet(mixins.CreateModelMixin,
 
 
 class TitleViewSet(viewsets.ModelViewSet):
-    queryset = models.Title.objects.all()
-    serializer_class = serializers.TitleSerializer
-    permission_classes = (permissions.IsAdminOrReadOnly,)
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (IsAdminOrReadOnly,)
     pagination_class = pagination.LimitOffsetPagination
     filter_backends = (DjangoFilterBackend,)
     filterset_fields = ('category__slug', 'genre__slug', 'name', 'year',)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        user = self.request.user
+        serializer.save(author=user, title=title)
 
     def get_queryset(self):
         return models.Title.objects.annotate(
             rating=django_models.Avg('reviews__rating')
         ).order_by('-avg_rating')
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentsSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        title = get_object_or_404(Title, pk=title_id)
+        review = get_object_or_404(Review, pk=review_id)
+        return Comments.objects.filter(title=title, review=review).all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        title = get_object_or_404(Title, pk=title_id)
+        review = get_object_or_404(Review, pk=review_id)
+        serializer.save(author=self.request.user, title=title, review=review)
