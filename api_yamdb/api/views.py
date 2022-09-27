761bfcd0115475324828d4d964a7dfa831b92cb8
Leonid Lovsky <leonid.lovsky@yandex.ru>
2022-09-27 19:54:38 +0300

    Набросок команды для импорта базы данных из csv файлов.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -12,6 +12,7 @@ from rest_framework.permissions import (
     IsAuthenticated
 )
 from rest_framework.response import Response
+
 from api.permissions import (
     IsAdminOrReadOnly,
     IsAdmin,
@@ -111,18 +112,18 @@ class CommentViewSet(viewsets.ModelViewSet):
     filter_backends = set()
 
     def get_queryset(self):
-        title_id = self.kwargs.get('title_id')
+        # title_id = self.kwargs.get('title_id')
         review_id = self.kwargs.get('review_id')
-        title = get_object_or_404(Title, pk=title_id)
+        # title = get_object_or_404(Title, pk=title_id)
         review = get_object_or_404(Review, pk=review_id)
-        return Comments.objects.filter(title=title, review=review)
+        return Comments.objects.filter(review=review)
 
     def perform_create(self, serializer):
-        title_id = self.kwargs.get('title_id')
+        # title_id = self.kwargs.get('title_id')
         review_id = self.kwargs.get('review_id')
-        title = get_object_or_404(Title, pk=title_id)
+        # title = get_object_or_404(Title, pk=title_id)
         review = get_object_or_404(Review, pk=review_id)
-        serializer.save(author=self.request.user, title=title, review=review)
+        serializer.save(author=self.request.user, review=review)
 
 
 class UserViewSet(viewsets.ModelViewSet):
